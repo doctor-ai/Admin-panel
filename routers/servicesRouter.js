d56a5acee3a989af8c0c8b235ecff18bef46c833
panther <panther@localhost.localdomain>
2019-07-16 00:06:19 +0530

    Code Updated

--- routers/servicesRouter.js
+++ routers/servicesRouter.js
@@ -2,7 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/",(req,res,next)=>{
-    res.render("pages/services")
+    res.render("pages/services/home")
 })
 
+router.get('/deseasefinder',(req,res,next)=>{
+    res.render("pages/services/deseaseFinder/home")
+})
+
+router.get('/deseasefinder/add',(req,res,next)=>{
+    res.render("pages/services/deseaseFinder/addDesease")
+})
+
+router.get('/deseasefinder/update',(req,res,next)=>{
+    res.render("pages/services/deseaseFinder/updateDesease")
+})
+
+router.get('/deseasefinder/list',(req,res,next)=>{
+    res.render("pages/services/deseaseFinder/list")
+})
 module.exports = router;
